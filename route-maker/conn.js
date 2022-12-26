c3626fcd4ad974b95efd4a6ecbdb2f40bbe077e7
Daniel Proença <daniel.proenca@lbrtelecom.com.br>
2022-12-26 14:46:56 -0300

    Class for easy crud!

--- route-maker/conn.js
+++ route-maker/conn.js
@@ -34,9 +34,10 @@ class Connection{
 
     async get_result(query, params=null){
 
+
     try{
         const rows = await this.pool.query(query, params);
-        return rows;
+        return rows[0];
 
     }catch(err){
         
@@ -48,13 +49,15 @@ class Connection{
 
     async insert(table,obj){
 
+
         if(typeof(obj) == 'object'){0
 
             if(!Array.isArray(obj)){
 
             const colums = Object.keys(obj)
             const values = Object.values(obj)
-            const req = await this.pool.query(`INSERT INTO ${table} (${colums.join(',')}) values (${values.map(e=>`'${e}'`).join(',')})`)
+            const req = await this.pool.query(`INSERT INTO ${table} (${colums.map(e=>'`'+`${e}`+'`').join(',')}) values (${values.map(e=>`'${e}'`).join(',')})`)
+            return req
             
             }else{
 
@@ -64,10 +67,12 @@ class Connection{
                 const colums = Object.keys(o)
                 const values = Object.values(o)
                 const req = await this.pool.query(`INSERT INTO ${table} (${colums.join(',')}) values (${values.map(e=>`'${e}'`).join(',')})`)        
+                return req
                 }
             catch(err){
                 console.log(err)
                 this.errors.push(err)
+                return err
             }
             })
             }
@@ -75,16 +80,88 @@ class Connection{
 
     }
 
+
+    async update(table, obj, where=false){
+        try{
+
+            const colums = Object.keys(obj)
+            const values = Object.values(obj)
+
+            const params = colums.map((e,i)=>`${'`'+e+'`'}='${values[i]}'`).join(',')
+
+            const req = await this.pool.query(`UPDATE ${table} SET ${params} ${where?'WHERE '+ where:''}`)
+
+            return req
+            
+        }catch(err){
+                console.log(err)
+                this.errors.push(err)
+                return err
+    }
+
 }
 
-async function teste(){
+    async delete(table, where){
+
+        try{
+
+       
+            
+            const req = await this.pool.query(`DELETE FROM ${table} ${'WHERE '+ where}`)
+
+            return req
+            
+        }catch(err){
+                console.log(err)
+                this.errors.push(err)
+                return err
+
+    }
+
+
+    }
+
+}
+
+
+/*Example*/
+
+/**
+ * Create a JSON file like "config.json" in this folder:
+ * 
+ * {
+
+"servers":[{
+    "name": "example",
+    "host": "localhost",
+    "user": "root",
+    "password": "pass",
+    "db": "mydb" 
+}]    
+}
+ */
+
+
+
+/*async function test(){
     const conn = new Connection(config.servers[0])
 
     conn.connect()
 
-    const res = await conn.insert({char:"teste"})
+    #INSERT
+    const res = await conn.insert("test",[{column:'value', column2:'other'}, {column:'value2', column2:'other'}])
+
+    #UPDATE
+    const res = await conn.insert("test",{column:value3},"`column` = 'value2'")
+
+    #DELETE
+    const res = await conn.delete("test", "`column` = 'test3'")
 
     console.log(res)
+
 }
 
+# RUN!!!!
+
 teste()
+*/
